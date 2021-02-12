0e8bd481c345fdc52aa90d2b19ea5e609803062b
Ayhan Kara <78213760+iayka@users.noreply.github.com>
2021-02-12 16:56:09 +0300

    Coin
    Oyun sonu kazanılacak coin scripti.

---
+++ Assets/Scripts/Collectible/Coin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    void Collectible()
+    {
+        FindObjectOfType<CoinManager>().AddScore(1);
+    }
+}
---
+++ Assets/Scripts/Collectible/CoinManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+    public Text mentosText;
+
+    private int coin;
+
+    private void Awake()
+    {
+        coin = PlayerPrefs.GetInt("mentos score değeri");
+        mentosText = coin.ToString();
+    }
+
+    public void AddScore(int amount)
+    {
+        coin += amount;
+        mentosText = coin.ToString();
+
+
+        PlayerPrefs.SetInt("mentos score değeri", coin);
+    }
+}
