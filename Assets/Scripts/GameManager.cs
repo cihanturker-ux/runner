f23d4ce8c9bd3cef9cab3a335cd1ded1d0d50e7a
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-02-12 21:59:28 +0300

    Thirtwo
    Movement scripts, collectibles, obstacles,game manager.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool isGameStarted = false;
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            isGameStarted = true;
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+namespace thirtwo.Scripts.PlayerController
+{
+
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        [Header("Components")]
+        private Rigidbody rb;
+        [Header("Character Info")]
+        [SerializeField] private float turnSpeed;
+        [SerializeField] private float runSpeed;
+        private float horizontal;
+        private Vector3 direction;
+        public int mentosCount;
+        
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+        }
+        private void Update()
+        {
+            if (GameManager.isGameStarted)
+            {
+                horizontal = Input.GetAxis("Horizontal");
+                direction = new Vector3(horizontal, 0, 0);
+            }
+        }
+        private void FixedUpdate()
+        {
+            if (GameManager.isGameStarted)
+            {
+                Move();
+            }
+        }
+
+        private void Move()
+        {
+            rb.AddForce(0, 0, runSpeed, ForceMode.Force);
+            rb.AddForce(direction * Time.fixedDeltaTime * turnSpeed, ForceMode.Impulse);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            
+             if(collision.collider.tag == "Obstacle")
+            {
+                if(mentosCount > 0)
+                {
+                    collision.gameObject.SetActive(false);
+                    //çarpma animasyonu
+                    transform.GetChild(mentosCount).gameObject.SetActive(false);
+                }
+                else
+                {
+                    GameManager.isGameStarted = false;
+                    // yanma animasyonu vs.
+                }
+            }
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Collectible")
+            {
+                mentosCount++;
+                other.gameObject.SetActive(false);
+                //toplama animasyonu
+                transform.GetChild(mentosCount).gameObject.SetActive(true);
+            }
+        }
+    }
+}
