2bef3f26c646a10f929638e62323451c246fd900
thirtwo <65511956+thirtwo@users.noreply.github.com>
2021-02-17 18:29:14 +0300

    Obstacle Rotation Script
    Obstacle Rotation Script

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [Tooltip("You can arrange rotation speed")]
+    [SerializeField] private float rotationTime;
+    [SerializeField] private float rotationSpeed;
+
+    void Start()
+    {
+        StartCoroutine(RotateObstacle());
+
+    }
+
+    private IEnumerator RotateObstacle()
+    {
+        while (true)
+        {
+            transform.Rotate(new Vector3(transform.eulerAngles.x, rotationSpeed * Time.deltaTime, transform.eulerAngles.z));
+            yield return new WaitForSeconds(rotationTime);
+        }
+    }
+}
